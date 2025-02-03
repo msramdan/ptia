c2b115cb10ee03890fa63ccf65cbff2c06d7db4c
Saepul Ramdan <saepulramdan244@gmail.com>
2025-02-03 11:50:35 +0700

    update

--- database/seeders/PesanWaSeeder.php
+++ database/seeders/PesanWaSeeder.php
@@ -13,7 +13,7 @@ class PesanWaSeeder extends Seeder
     public function run(): void
     {
         DB::table('pesan_wa')->insert([
-            'text_pesan' => "<p>*Yth. Bapak/Ibu Alumni *</p><p>Dalam rangka evaluasi pasca pembelajaran atas , kami harapkan partisipasi Bapak/Ibu sebagai *alumni* pelatihan untuk mengisi kuesioner melalui link berikut ini:</p><p>&lt;link&gt;</p><p>Paling lambat tanggal *15 Januari 2025*.</p><p>Pusdiklatwas BPKP menjamin kerahasiaan informasi yang diberikan.</p><p>Narahubung berkenaan evaluasi ini dapat menghubungi nomor WA 085640792084 a.n. Sdr. Cahyo Radityo Terima kasih.&nbsp;</p><p>*Tim Evaluasi Pusdiklatwas BPKP*&nbsp;</p><p>&nbsp;</p><p>Catatan: Jika link tidak bisa diklik, mohon terlebih dahulu menyimpan nomor GIA Corpu ini ke dalam daftar kontak Bapak/Ibu.</p>"
+            'text_pesan' => "<p>*Yth. Bapak/Ibu Alumni *</p><p>Dalam rangka evaluasi pasca pembelajaran atas *{params_judul_diklat}*, kami harapkan partisipasi Bapak/Ibu sebagai *{params_target}* pelatihan untuk mengisi kuesioner melalui link berikut ini:</p><p>*{params_link}*</p><p>Paling lambat tanggal *{params_deadline}*.</p><p>Pusdiklatwas BPKP menjamin kerahasiaan informasi yang diberikan.</p><p>Narahubung berkenaan evaluasi ini dapat menghubungi nomor WA *{params_wa_pic}* a.n. Sdr. *{params_pic}* Terima kasih.&nbsp;</p><p>*Tim Evaluasi Pusdiklatwas BPKP*&nbsp;</p><p>&nbsp;</p><p>Catatan: Jika link tidak bisa diklik, mohon terlebih dahulu menyimpan nomor GIA Corpu ini ke dalam daftar kontak Bapak/Ibu.</p>"
         ]);
     }
 }
