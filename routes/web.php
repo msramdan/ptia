8f38e7c18bad4bdd869a77e75ec27fdcfb3deaf1
Saepul Ramdan <saepulramdan244@gmail.com>
2025-02-04 14:05:40 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,46 +14,47 @@ use App\Http\Controllers\{
     SingleSenderController,
     IndikatorDampakController,
     KonversiController,
-    ProjectController,
-    PembuatanProjectController
+    PembuatanProjectController,
+    ProjectController
 };
 
-Route::get('/', fn () => view('welcome'));
+Route::get('/', fn() => view('welcome'));
 
 Route::middleware(['auth', 'web'])->group(function () {
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
     Route::get('/profile', ProfileController::class)->name('profile');
 
     Route::resources([
-        'users'               => UserController::class,
-        'roles'               => RoleAndPermissionController::class,
-        'aspek'               => AspekController::class,
-        'indikator-persepsi'  => IndikatorPersepsiController::class,
-        'pesan-wa'            => PesanWaController::class,
-        'bobot-aspek'         => BobotAspekController::class,
-        'kriteria-responden'  => KriteriaRespondenController::class,
-        'wa-blast'            => WaBlastController::class,
-        'single-sender'       => SingleSenderController::class,
-        'indikator-dampak'    => IndikatorDampakController::class,
-        'konversi'            => KonversiController::class,
-        'project'             => ProjectController::class,
-        'pembuatan-project'   => PembuatanProjectController::class,
+        'users' => UserController::class,
+        'roles' => RoleAndPermissionController::class,
+        'aspek' => AspekController::class,
+        'indikator-persepsi' => IndikatorPersepsiController::class,
+        'pesan-wa' => PesanWaController::class,
+        'bobot-aspek' => BobotAspekController::class,
+        'kriteria-responden' => KriteriaRespondenController::class,
+        'wa-blast' => WaBlastController::class,
+        'single-sender' => SingleSenderController::class,
+        'indikator-dampak' => IndikatorDampakController::class,
+        'konversi' => KonversiController::class,
+        'project' => ProjectController::class,
+        'pembuatan-project' => PembuatanProjectController::class,
     ]);
 
-    Route::post('/update-session-status', [WaBlastController::class, 'updateSessionStatus'])
-        ->name('update.session.status');
-
-    Route::prefix('get-kaldik-data')->group(function () {
-        Route::get('/', [PembuatanProjectController::class, 'getKaldikData'])->name('kaldik.index');
-        Route::get('/detail/{kaldikID}', [PembuatanProjectController::class, 'getDetail'])->name('kaldik.detail');
+    Route::post('/update-session-status', [WaBlastController::class, 'updateSessionStatus'])->name('update.session.status');
+    Route::prefix('get-kaldik-data')->controller(PembuatanProjectController::class)->group(function () {
+        Route::get('/', 'getKaldikData')->name('kaldik.index');
+        Route::get('/detail/{kaldikID}', 'getDetail')->name('kaldik.detail');
     });
 
-    Route::prefix('project/kuesioner')->name('kuesioner.')->group(function () {
-        Route::get('/show/{id}/{remark}', [ProjectController::class, 'showKuesioner'])->name('show');
-        Route::post('/store', [ProjectController::class, 'tambahKuesioner'])->name('store');
-        Route::get('/edit/{id}', [ProjectController::class, 'editKuesioner'])->name('edit');
-        Route::post('/update/{id}', [ProjectController::class, 'saveKuesioner'])->name('update');
-        Route::delete('/delete/{id}', [ProjectController::class, 'deleteKuesioner'])->name('delete');
-        Route::get('/show-responden/{id}', [ProjectController::class, 'showResponden'])->name('responden.show');
+    Route::prefix('project/kuesioner')->controller(ProjectController::class)->group(function () {
+        // Kuesioner
+        Route::get('/show/{id}/{remark}', 'showKuesioner')->name('kuesioner.show');
+        Route::post('/store', 'tambahKuesioner')->name('kuesioner.store');
+        Route::get('/edit/{id}', 'editKuesioner')->name('kuesioner.edit');
+        Route::post('/update/{id}', 'saveKuesioner')->name('kuesioner.update');
+        Route::delete('/delete/{id}', 'deleteKuesioner')->name('kuesioner.delete');
+        // Peserta
+        Route::get('/show-responden/{id}', 'showResponden')->name('responden.show');
     });
+
 });
