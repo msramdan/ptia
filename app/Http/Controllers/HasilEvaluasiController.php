e1ebc3565fdc79db3094dc45b741b1d761cc69c0
Saepul Ramdan <saepulramdan244@gmail.com>
2025-03-18 20:06:53 +0700

    update add least 100

--- app/Http/Controllers/HasilEvaluasiController.php
+++ app/Http/Controllers/HasilEvaluasiController.php
@@ -37,18 +37,47 @@ class HasilEvaluasiController extends Controller implements HasMiddleware
                     'users.avatar as user_avatar',
                     'users.email as user_email',
                     DB::raw('COALESCE(avg_scores.avg_skor_level_3, 0) AS avg_skor_level_3'),
-                    DB::raw('COALESCE(avg_scores.avg_skor_level_4, 0) AS avg_skor_level_4'),
+                    DB::raw('COALESCE(avg_scores.final_avg_skor_level_4, 0) AS avg_skor_level_4'),
                     DB::raw("COALESCE(indikator_3.kriteria_dampak, '-') AS kriteria_dampak_level_3"),
                     DB::raw("COALESCE(indikator_4.kriteria_dampak, '-') AS kriteria_dampak_level_4")
                 )
                 ->leftJoinSub(
                     DB::table('project_skor_responden')
                         ->select(
-                            'project_id',
-                            DB::raw("COALESCE(ROUND(AVG((COALESCE(skor_level_3_alumni, 0) + COALESCE(skor_level_3_atasan, 0))), 2), 0) AS avg_skor_level_3"),
-                            DB::raw("COALESCE(ROUND(AVG((COALESCE(skor_level_4_alumni, 0) + COALESCE(skor_level_4_atasan, 0))), 2), 0) AS avg_skor_level_4")
+                            'project_skor_responden.project_id',
+                            DB::raw("LEAST(100, COALESCE(ROUND(AVG((COALESCE(skor_level_3_alumni, 0) + COALESCE(skor_level_3_atasan, 0))), 2), 0)) AS avg_skor_level_3"),
+                            DB::raw("LEAST(100, COALESCE(ROUND(AVG((COALESCE(skor_level_4_alumni, 0) + COALESCE(skor_level_4_atasan, 0))), 2), 0)) AS base_avg_skor_level_4"),
+                            DB::raw(
+                                "COALESCE((
+                                SELECT bobot_aspek_sekunder
+                                FROM project_bobot_aspek_sekunder
+                                WHERE project_bobot_aspek_sekunder.project_id = project_skor_responden.project_id
+                                AND EXISTS (
+                                    SELECT 1
+                                    FROM project_data_sekunder
+                                    WHERE project_data_sekunder.project_id = project_skor_responden.project_id
+                                    AND project_data_sekunder.nilai_kinerja_awal < project_data_sekunder.nilai_kinerja_akhir
+                                )
+                            ), 0) AS bobot_aspek_sekunder"
+                            ),
+                            DB::raw(
+                                "LEAST(100,
+                                (COALESCE(ROUND(AVG((COALESCE(skor_level_4_alumni, 0) + COALESCE(skor_level_4_atasan, 0))), 2), 0) +
+                                COALESCE((
+                                    SELECT bobot_aspek_sekunder
+                                    FROM project_bobot_aspek_sekunder
+                                    WHERE project_bobot_aspek_sekunder.project_id = project_skor_responden.project_id
+                                    AND EXISTS (
+                                        SELECT 1
+                                        FROM project_data_sekunder
+                                        WHERE project_data_sekunder.project_id = project_skor_responden.project_id
+                                        AND project_data_sekunder.nilai_kinerja_awal < project_data_sekunder.nilai_kinerja_akhir
+                                    )
+                                ), 0))
+                            ) AS final_avg_skor_level_4"
+                            )
                         )
-                        ->groupBy('project_id'),
+                        ->groupBy('project_skor_responden.project_id'),
                     'avg_scores',
                     'project.id',
                     '=',
@@ -59,21 +88,22 @@ class HasilEvaluasiController extends Controller implements HasMiddleware
                 ->leftJoin('indikator_dampak AS indikator_3', function ($join) {
                     $join->on('project.diklat_type_id', '=', 'indikator_3.diklat_type_id')
                         ->whereRaw('
-                            COALESCE(avg_scores.avg_skor_level_3, 0) > indikator_3.nilai_minimal
-                            AND
-                            COALESCE(avg_scores.avg_skor_level_3, 0) <= indikator_3.nilai_maksimal
-                        ');
+                        COALESCE(avg_scores.avg_skor_level_3, 0) > indikator_3.nilai_minimal
+                        AND
+                        COALESCE(avg_scores.avg_skor_level_3, 0) <= indikator_3.nilai_maksimal
+                    ');
                 })
                 ->leftJoin('indikator_dampak AS indikator_4', function ($join) {
                     $join->on('project.diklat_type_id', '=', 'indikator_4.diklat_type_id')
                         ->whereRaw('
-                            COALESCE(avg_scores.avg_skor_level_4, 0) > indikator_4.nilai_minimal
-                            AND
-                            COALESCE(avg_scores.avg_skor_level_4, 0) <= indikator_4.nilai_maksimal
-                        ');
+                        COALESCE(avg_scores.final_avg_skor_level_4, 0) > indikator_4.nilai_minimal
+                        AND
+                        COALESCE(avg_scores.final_avg_skor_level_4, 0) <= indikator_4.nilai_maksimal
+                    ');
                 })
                 ->where('project.status', 'Pelaksanaan')
                 ->orderByDesc('project.id');
+
 
             return DataTables::of($projects)
                 ->addIndexColumn()
@@ -125,7 +155,6 @@ class HasilEvaluasiController extends Controller implements HasMiddleware
                         ');
                 })
                 ->where('project_skor_responden.project_id', $id)
-
                 ->select([
                     'project_skor_responden.project_id',
                     'project_skor_responden.project_responden_id',
@@ -138,15 +167,16 @@ class HasilEvaluasiController extends Controller implements HasMiddleware
                     'project_responden.unit',
                     'project.diklat_type_id',
                     DB::raw('
-                ROUND(
-                    (COALESCE(project_skor_responden.skor_level_3_alumni, 0) +
-                     COALESCE(project_skor_responden.skor_level_3_atasan, 0)),
-                    2
-                ) AS avg_skor_level_3
-            '),
+                        LEAST(100, ROUND(
+                            (COALESCE(project_skor_responden.skor_level_3_alumni, 0) +
+                             COALESCE(project_skor_responden.skor_level_3_atasan, 0)),
+                            2
+                        )) AS avg_skor_level_3
+                    '),
                     'indikator_dampak.kriteria_dampak'
                 ])
                 ->get();
+
             return DataTables::of($data)->addIndexColumn()->toJson();
         }
         return view('hasil-evaluasi.detail-skor-level3', compact('project'));
